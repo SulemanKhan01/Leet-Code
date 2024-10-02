a828d84a20b007781a2240b69d231ec6b2fd94d5
Suleman Khan <168589213+SulemanKhan01@users.noreply.github.com>
2024-10-02 22:31:14 +0500

    CODE

---
+++ top_k.py
@@ -0,0 +1,40 @@
+def top_k_elements(nums , k):
+    hash_table = {}
+    for num in nums:
+        if num in hash_table:
+            hash_table[num] +=1
+        else:
+            hash_table[num] = 1
+
+    sorted_dict = sorted(hash_table.items() , key=lambda item: item[1])
+    sorted_dict.reverse()
+
+    top_keys = sorted_dict[:k]
+
+    final = []
+    for key , value in top_keys:
+        final.append(key)
+
+    return final
+
+
+    # return hash_table
+    # values = list(hash_table.values())
+    # values.sort()
+    # values.reverse()
+    # goal = values[:k]
+    # final = []
+    # for key in goal:
+    #     final_keys = hash_table.get(key)
+    #     final.append(final_keys)
+
+    return final
+
+        
+
+
+
+
+nums = [1,2,2,2,2,2,2,2,3,3,4,5,5,5,5]
+k = 3
+print(top_k_elements(nums , k))
